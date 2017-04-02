e936c51d9a8ae0f34250a810defcda1c6c8c6c9f
Forzcross <forzcross@gmail.com>
2017-04-02 21:29:34 +0300

    alpha bd state

--- lab4/DataBase.cs
+++ lab4/DataBase.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 
 namespace lab4
 {
-    class DataBase
+    class ImageDataBase
     {
+        string dbPath;
+        SQLiteConnection connection;
+        public ImageDataBase(string dbname)
+        {
+            dbPath = dbname;
+            if (!File.Exists(dbname))
+            {
+                SQLiteConnection.CreateFile(dbname);
+                connection = new SQLiteConnection(string.Format("Data Source={0};", dbname));
+            }            
+        }
     }
 }
