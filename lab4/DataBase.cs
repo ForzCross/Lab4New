d7db61c9bd303bcd600e33dd386936153b17f66c
Forzcross <forzcross@gmail.com>
2017-04-02 21:30:52 +0300

    добавил цепочку в солюшон

--- lab4/DataBase.cs
+++ lab4/DataBase.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.IO;
+using System.Drawing;
 
 namespace lab4
 {
@@ -19,7 +20,33 @@ namespace lab4
             {
                 SQLiteConnection.CreateFile(dbname);
                 connection = new SQLiteConnection(string.Format("Data Source={0};", dbname));
+                connection.Open();
+
             }            
+        }
+
+        int execWrite(string query)
+        {
+            SQLiteCommand command = new SQLiteCommand(query, connection);
+            return command.ExecuteNonQuery();           
+        }
+        SQLiteDataReader execRead(string query)
+        {
+            SQLiteCommand command = new SQLiteCommand(query, connection);
+            return command.ExecuteReader();
+        }
+        public bool storeImage(Bitmap img)
+        {
+            byte[] data = bitmapToByteArray(img);
+            //string sqlquery = "SELECT * FROM "
+            //execWrite()
+            return true;            
+        }
+
+        public static byte[] bitmapToByteArray(Bitmap img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
         }
     }
 }
